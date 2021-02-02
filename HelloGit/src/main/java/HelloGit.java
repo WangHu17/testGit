c88194cf0b8fa307738c1097a868ea22b1987183
WangHu17 <1936749601@qq.com>
2021-02-02 20:49:06 +0800

    第一次提交

---
+++ HelloGit/src/main/java/HelloGit.java
@@ -0,0 +1,10 @@
+/**
+ * @author wanghu
+ * @discrption：
+ * @create 2021-02-02 16:59
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("HelloGit");
+    }
+}
