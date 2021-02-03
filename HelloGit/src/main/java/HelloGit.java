8e60152acdd97ea0e88256cb3d587511de7e8a14
WangHu17 <1936749601@qq.com>
2021-02-03 16:05:56 +0800

    更新2

--- HelloGit/src/main/java/HelloGit.java
+++ HelloGit/src/main/java/HelloGit.java
@@ -7,5 +7,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("HelloGit");
         System.out.println("更新1");
+        System.out.println("更新2");
     }
 }
